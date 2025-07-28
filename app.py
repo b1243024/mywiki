3ee4e8185a3a4fac6bd81bd99194295439c3de3c
鄭立欣 <144189399+b1243024@users.noreply.github.com>
2025-07-28 20:56:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -72,6 +72,8 @@ def new_article():
 if __name__ == '__main__':
     init_db()
     app.run(debug=True, host='0.0.0.0', port=5000)
+else:
+    init_db()  # 加這行，Render 才會建資料庫
 
 def init_db():
     if not os.path.exists(DB_NAME):
